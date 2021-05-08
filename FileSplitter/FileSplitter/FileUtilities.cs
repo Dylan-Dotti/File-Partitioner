fa1b43b228bca911fec63629ed94bb4984674ec5
Dylan Dotti <invocation42@yahoo.com>
2021-05-08 17:10:04 -0400

    Added to FileUtilities

--- FileSplitter/FileSplitter/FileUtilities.cs
+++ FileSplitter/FileSplitter/FileUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 
 namespace FileSplitter
@@ -15,5 +16,21 @@ namespace FileSplitter
         public static void MoveFile(string srcPath, string dstPath) => File.Move(srcPath, dstPath);
 
         public static void CreateDirectory(string path) => Directory.CreateDirectory(path);
+
+        public static ZipArchive CreateZipArchive(string path) => ZipFile.Open(path, ZipArchiveMode.Create);
+
+        public static ZipArchive ZipFiles(string archivePath, IEnumerable<string> filePaths,
+            Action<ZipArchiveEntry> OnEntryCreated = null)
+        {
+            using (ZipArchive archive = CreateZipArchive(archivePath))
+            {
+                foreach (string path in filePaths)
+                {
+                    var entry = archive.CreateEntryFromFile(path, Path.GetFileName(path));
+                    OnEntryCreated?.Invoke(entry);
+                }
+                return archive;
+            }
+        }
     }
 }
